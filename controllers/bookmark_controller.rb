2e1275904bf019197f1bf1404793e311d5788ca5
User <user@users-mbp-8.home>
2015-10-23 19:31:21 +0100

    23/10/15

--- controllers/bookmark_controller.rb
+++ controllers/bookmark_controller.rb
@@ -2,3 +2,9 @@ get '/bookmarks/new' do
     @bookmark = Bookmark.new
     erb :'bookmarks/new'
   end
+
+  get '/bookmarks' do
+  @bookmark = Bookmark.all
+  erb :'bookmarks/index'
+end
+
--- models/db_base.rb
+++ models/db_base.rb
@@ -1,16 +1,6 @@
 class DBBase
 
   attr_accessor :id
-  
-def self.run_sql(sql)
-    conn = PG.connect(dbname: 'bookmark', host: 'localhost')
-    begin
-      result = conn.exec(sql)
-    ensure
-      conn.close
-    end
-    result
-  end
 
   def self.attributes(attrs)
     @attributes = attrs
@@ -19,9 +9,115 @@ def self.run_sql(sql)
     end
   end
 
+  def self.get_attributes
+    @attributes
+  end
 
+  def self.table_name
+    @table_name ||= begin
+      case
+        when name[-1] == 'y'
+          name[0...-1] + 'ies'
+        when name[-1] == 'x'
+          name + 'es'
+        when name[-2..-1] == 'sh'
+          name + 'es'
+        else
+          name + 's'
+      end.downcase
+    end
+  end
 
+  def self.run_sql(sql)
+    conn = PG.connect(dbname: 'bookmarks', host: 'localhost')
+    begin
+      result = conn.exec(sql)
+    ensure
+      conn.close
+    end
+    result
+  end
+
+  def self.all
+    results = run_sql("SELECT * FROM #{table_name}")
+    results.map { |result| self.new(result) }
+  end
+
+  def self.find(id)
+    result = run_sql("SELECT * FROM #{table_name} WHERE id = #{sql_sanitize(id, :integer)}").first
+    self.new(result) if result
+  end
+
+  def self.sql_sanitize(value, type)
+    case type
+      when :string
+        "'#{value.to_s.gsub("'", "''")}'"
+      when :text
+        "'#{value.to_s.gsub("'", "''")}'"
+      when :integer
+        value.to_i
+      when :decimal
+        value.to_f
+      else
+        raise "Unrecognised data type `#{type}`"
+    end
+  end
+
+  def initialize(params={})
+    params.keys.each do |key|
+      instance_variable_set("@#{key}", params[key])
+    end
+  end
+
+  def attributes
+    self.class.get_attributes
+  end
+
+  def table_name
+    self.class.table_name
+  end
+
+  def run_sql(sql)
+    self.class.run_sql(sql)
+  end
+
+  def sql_sanitize(value, type)
+    self.class.sql_sanitize(value, type)
+  end
+
+  def save
+    if id.nil?
+      sql_fields = []
+      sql_values = []
+      attributes.each do |attribute, type|
+        sql_fields << attribute
+        sql_values << sql_sanitize(self.send(attribute), type)
+      end
+
+      sql = "INSERT INTO #{table_name} (#{sql_fields.join(', ')}) VALUES (#{sql_values.join(', ')}) RETURNING id"
+      self.id = run_sql(sql).first['id']
+
+    else
+
+      sql_fields_and_values = attributes.map do |attribute, type|
+        "#{attribute} = #{sql_sanitize(self.send(attribute), type)}"
+      end
+
+      sql = "UPDATE #{table_name} SET #{sql_fields_and_values.join(', ')} WHERE id = #{sql_sanitize(id, :integer)}"
+      run_sql(sql)
+    end
+
+  end
+
+  def update_attributes(attrs)
+    attrs.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    save
+  end
+
+  def destroy
+    run_sql("DELETE FROM #{table_name} WHERE id = #{sql_sanitize(id, :integer)}")
+  end
 
 end
-
-
